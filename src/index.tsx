ee112b10ef066844425ffd4d35b6df2aaa09ebfa
florianboudot <florian.boudot@gmail.com>
2022-10-17 18:33:14 +0200

    add simple mesh to test

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import ReactDOM from 'react-dom';
 import { Canvas } from '@react-three/fiber';
+import './index.css';
 
 function App() {
   return (
-    <div id="canvas-container">
-      <Canvas>lol</Canvas>
-    </div>
+    <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 5] }}>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
+      <mesh>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial />
+      </mesh>
+    </Canvas>
   );
 }
 
