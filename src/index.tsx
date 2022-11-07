581d3d4f7aa34e1f8d2c240996b49136c8a38b58
florianboudot <florian.boudot@gmail.com>
2022-11-07 21:52:27 +0100

    set orbit controls and ortho cam with debug ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,87 @@
+import { useControls } from 'leva';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useMemo } from 'react';
+import { Canvas, MeshProps } from '@react-three/fiber';
 import * as THREE from 'three';
-import { OrbitControls, OrthographicCamera } from '@react-three/drei';
+import { Box, OrbitControls, OrthographicCamera } from '@react-three/drei';
+
+const NUM = 3;
+interface Positions {
+  id: string;
+  position: [number, number, number];
+}
+
+interface BuildingProps extends MeshProps {
+  storeys?: number;
+}
+const Building = ({ position, storeys = 1 }: BuildingProps) => {
+  const positions = useMemo(() => {
+    const pos: Positions[] = [];
+    const half = (NUM - 1) / 2;
+
+    for (let x = 0; x < NUM; x++) {
+      for (let y = 0; y < NUM; y++) {
+        pos.push({
+          id: `${x}-${y}`,
+          position: [(x - half) * 4, (y - half) * 4, 0],
+        });
+      }
+    }
+
+    return pos;
+  }, []);
+
+  return (
+    <>
+      {positions.map(({ id, position }) => (
+        <Box key={id} position={position}>
+          <meshBasicMaterial color="white" side={THREE.DoubleSide} />
+        </Box>
+      ))}
+    </>
+  );
+};
+
+const OrthoCam = () => {
+  const { camX, camY, camZ, zoom } = useControls({
+    camX: { value: 1, min: -50, max: 50, step: 5 },
+    camY: { value: 0.542, min: -50, max: 50, step: 5 },
+    camZ: { value: 1, min: -50, max: 50, step: 5 },
+    zoom: { value: 20, min: 0, max: 50, step: 1 },
+  });
+
+  return (
+    <OrthographicCamera
+      makeDefault
+      position={[camX, camY, camZ]}
+      zoom={zoom}
+      near={-200}
+      far={4000}
+    />
+  );
+};
 
 function App() {
   return (
     <Canvas>
-      <OrthographicCamera
-        makeDefault
-        position={[0, 0, 10]}
-        zoom={5}
-        near={-200}
-        far={4000}
-      />
+      <OrthoCam />
 
-      <mesh position={[2, -8, 2]} rotation={[Math.PI * 0.5, 0, 0]}>
+      <Building position={[0, 0, 0]} />
+      <mesh position={[0, 0, 0]}>
         <planeGeometry args={[200, 200, 75, 75]} />
         <meshBasicMaterial wireframe color="white" side={THREE.DoubleSide} />
       </mesh>
-      <OrbitControls />
+      <OrbitControls
+        // autoRotate={false}
+        enableRotate={false}
+        screenSpacePanning={true}
+        mouseButtons={{
+          LEFT: THREE.MOUSE.PAN,
+          MIDDLE: THREE.MOUSE.DOLLY,
+          RIGHT: THREE.MOUSE.PAN,
+        }}
+      />
     </Canvas>
   );
 }
